f2683686d511a6b4f3239f1c89fa7a427af33fd3
deramsey <ramseyd@clevelandcc.edu>
2023-01-26 16:31:45 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://clevelandcc.edu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Cleveland Community College
         </a>
       </header>
     </div>
+
   );
 }
 
